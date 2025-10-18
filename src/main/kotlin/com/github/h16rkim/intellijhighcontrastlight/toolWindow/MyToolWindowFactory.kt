359db78db3de0ab82f0bfc0041f52e780dd7a29b
GitHub Action <action@github.com>
2025-10-18 12:04:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/h16rkim/intellijhighcontrastlight/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/h16rkim/intellijhighcontrastlight/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.h16rkim.intellijhighcontrastlight.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.h16rkim.intellijhighcontrastlight.MyBundle
+import com.github.h16rkim.intellijhighcontrastlight.services.MyProjectService
 import javax.swing.JButton
 
 
